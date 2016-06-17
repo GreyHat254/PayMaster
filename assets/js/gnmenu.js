52de0627ce41299edc8be977ac8e36d5553d446e
Tole <iantole@Ians-MacBook.local>
2016-06-17 16:28:42 +0300

    More
    More styling files, fonts and js files

---
+++ assets/js/gnmenu.js
@@ -0,0 +1,29 @@
+$(function() {
+    // Stick the #nav to the top of the window
+    var nav = $('#cssmenu');
+    var stickyNav = nav.offset().top;
+    var isFixed = false;
+    var $w = $(window);
+    $w.scroll(function() {
+        var scrollTop = $w.scrollTop();
+        var shouldBeFixed = scrollTop > stickyNav;
+        if (shouldBeFixed && !isFixed) {
+            nav.css({
+                position: 'fixed',
+                top: 0,
+                left: nav.offset().left,
+                width: nav.width()
+            });
+            isFixed = true;
+        }
+        else if (!shouldBeFixed && isFixed)
+        {
+            nav.css({
+                position: 'static'
+            });
+            isFixed = false;
+        }
+    });
+	
+	
+});
